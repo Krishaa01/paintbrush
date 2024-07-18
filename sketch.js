1ec63058629203784981f3444ffc2d06300064af
Krishaa01 <krisha.shah.sod23@anu.edu.in>
2024-07-18 15:58:01 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,16 @@
+function setup() {
+  createCanvas(500, 500);
+  
+  background(09,30,8)
+  
+}
+
+function draw() {
+
+  
+  rect(mouseX, mouseY, 56,32,77,85);
+  stroke(28,82,69);
+  fill(mouseX,mouseY, 55,80,20);
+  noStroke();
+  
+}
